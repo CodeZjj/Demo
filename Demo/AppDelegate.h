4442552cfb2c8dfc225141353d0c61d9baf8fb4b
zjj <931949892@qq.com>
2017-09-05 08:45:10 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by YaoYuSmart on 2017/9/5.
+//  Copyright © 2017年 YaoYuSmart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
